60e0d76bb01850691197b87b14e8c700b9a643b9
AraidaOS <102170899+AraidaOS@users.noreply.github.com>
2026-05-02 23:12:19 +0200

    Create solution.py

---
+++ Power Digit Sum #16/solution.py
@@ -0,0 +1,32 @@
+"""
+Problem 16 - Power Digit Sum
+
+2^15 = 32768 and the sum of its digits is 3+2+7+6+8 = 26.
+What is the sum of the digits of the number 2^1000?
+"""
+
+"""
+solution:
+    - compute 2^1000 usint pow().
+
+"""
+
+# :author: th4l3s
+
+
+def solve():
+    n = pow(2, 1000)
+    s = str(n)
+    res = 0
+    for c in s:
+        res += int(c)
+    return res
+
+def main():
+    solution = solve()
+    print(f"Solution: {solution}")
+
+
+# Usage:
+if __name__ == "__main__":
+    main()
